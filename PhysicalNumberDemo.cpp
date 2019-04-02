d6f2f18a0354852310b99c761dc41f27df38b588
BarGenish <44568486+bargenish44@users.noreply.github.com>
2019-04-02 13:19:59 +0300

    Update PhysicalNumberDemo.cpp

--- PhysicalNumberDemo.cpp
+++ PhysicalNumberDemo.cpp
@@ -22,7 +22,7 @@ int main() {
     cout << a << endl;   // Prints "2[km]"
     cout << (a+b) << endl;   // Prints "2.3[km]"
     cout << (b-a) << endl;   // Prints "-1700[m]"
-    // cout << (-a) << endl;   // Prints "-2[km]"
+    cout << (-a) << endl;   // Prints "-2[km]"
 
     cout << (a>b) << endl;  // Prints "true"
     cout << (a<=b) << endl;  // Prints "false"
